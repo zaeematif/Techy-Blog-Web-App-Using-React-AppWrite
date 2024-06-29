07e0d1f09e1efb82620f7a32a25fd16f99c86c36
Zaeem <zaeem@gmail.com>
2024-06-29 20:45:06 +0300

    Compiled and Exported all URLs in .env file

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import config from './config/config';
+
+function App() {
+
+
+  console.log(config.appWriteUrl);
+
+  return (
+    <>
+      <h1>Loading React...!</h1>
+    </>
+  )
+}
+
+export default App
